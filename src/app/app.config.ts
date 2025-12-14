c05f3486ee325d2f53eb0b095044145b2431df1f
Alba Casares Catala <albacasarescatala@Mac-mini-de-Alba.local>
2025-12-14 17:35:14 +0100

    Deploy Angular standalone app to GitHub Pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { withHashLocation, provideRouter } from '@angular/router';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withHashLocation())
+  ]
 };
+
+
